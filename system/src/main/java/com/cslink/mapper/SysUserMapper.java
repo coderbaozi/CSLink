1b8b58aaa678c0c2e7a9820c5e49969077fb4429
coderbaozi <YHbaozi1988@163.com>
2023-03-28 22:55:58 +0800

    :sql: add user sign in sql

--- system/src/main/java/com/cslink/mapper/SysUserMapper.java
+++ system/src/main/java/com/cslink/mapper/SysUserMapper.java
@@ -1,8 +1,10 @@
 package com.cslink.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cslink.domain.SysUser;
+import com.cslink.domain.vo.LoginUserVO;
 import com.cslink.domain.vo.SignupVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -16,4 +18,10 @@ public interface SysUserMapper extends BaseMapper<SysUser> {
     SignupVO existEmail(String email);
 
     Integer saveUser(SignupVO user);
+
+    LoginUserVO queryEmailByUsername(String username);
+
+    Integer queryUserNameAndPassword(@Param("username") String username,@Param("password") String password);
+
+    Integer queryEmailAndPassword(@Param("email") String email,@Param("password") String password);
 }
